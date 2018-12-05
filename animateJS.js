fa72ed66b30048fc32f6ef560d454c95d8011db7
ChaoticVisions <45571216+ChaoticVisions@users.noreply.github.com>
2018-12-05 12:03:04 -0600

    Update animateJS.js

--- animateJS.js
+++ animateJS.js
@@ -1,23 +1,14 @@
-var head  = document.getElementsByTagName('head')[0];
-var link  = document.createElement('link');
-link.rel  = 'stylesheet';
-link.type = 'text/css';
-link.href = 'https://raw.githubusercontent.com/ChaoticVisions/CoyoteCoding/master/animateCSS.scss';
-link.media = 'all';
-head.appendChild(link);
-var aJS = {
-  anim: function(item,animationID) {
-    if(animationID == 1) {
-      $(item).toggleClass('pulse');
-    }
-    if(animationID == 2) {
-	    $(item).toggleClass('bobble');
-    }
-    if(animationID == 3) {
-	    $(item).toggleClass('background-slide');
-    }
-    if(animationID == 4) {
-	    $(item).toggleClass('rectangle-out');
-    }
+function anim(item,animationID) {
+  if(animationID == 1) {
+    $(item).toggleClass('pulse');
+  }
+   if(animationID == 2) {
+	  $(item).toggleClass('bobble');
+  }
+   if(animationID == 3) {
+	  $(item).toggleClass('background-slide');
+  }
+   if(animationID == 4) {
+	  $(item).toggleClass('rectangle-out');
   }
 }
